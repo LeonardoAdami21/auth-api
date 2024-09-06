bc6447025512b34282719cebabb6d1d645b50264
LeonardoAdami21 <lolzinhobr1000@gmail.com>
2024-09-06 14:51:14 -0300

    refactor/auth, refatoração da implementação da api de autenticação e autorização.

--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -5,11 +5,34 @@ export class RegisterUserDto {
   @ApiProperty()
   name: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'User Document(CPF or CNPJ)',
+    example: '12345678901 or 12345678901234',
+    type: String
+  })
+  document: string;
+
+  @ApiProperty({
+    description: 'User email',
+    example: 'p8iX7@example.com',
+  })
   email: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'User password',
+    example: '123456',
+  })
   password: string;
+
+  @ApiProperty({
+    isArray: true,
+    description: 'User addresses',
+    example: [
+      'street 1, 123',
+      'street 2, 456'
+    ]
+  })
+  address: string[];
 
   role?: enumRole
 }
